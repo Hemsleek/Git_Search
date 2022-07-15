000b5116e369355dc70f5b8d2cb007713bf984a9
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-07-15 12:38:25 +0100

    feat: users search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,66 @@
+import axios from "axios";
 import { useState } from "react";
 import "./App.css";
 
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
 function App() {
-  return <div className="w-10 h-10 bg-red-200"></div>;
+  const [searchResult, setSearchResult] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handleUserSearch = async (e) => {
+    e.preventDefault();
+
+    const searchQuery = e.target.query.value;
+    const searchUrl = `${baseUrl}/search/users?q=${searchQuery}`;
+    setIsFetching(true);
+
+    try {
+      const { data } = await axios(searchUrl);
+      console.log(data);
+      setSearchResult(data.items);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleUserSearch}>
+        <input
+          type="search"
+          name="query"
+          placeholder="search"
+          className="searchInput"
+        />
+      </form>
+      <div className="display">
+        <span className="text-center ">Display</span>
+        {isFetching ? (
+          <span className="text-center font-bold text-lg">
+            Fetching users...
+          </span>
+        ) : (
+          <div className="Users flex flex-col gap-y-5">
+            {searchResult.map((user) => (
+              <div key={user.id} className="user flex items-center ">
+                <img
+                  src={user.avatar_url}
+                  alt="user-avatar"
+                  className="w-20 h-20 rounded-full"
+                />
+                <span className="ml-3 font-extrabold text-lg">
+                  {user.login}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
